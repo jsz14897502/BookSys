4d9cdc03b4f9befbdcc35c2944ba81e518380b4c
Gridsah <gridslc45@gmail.com>
2018-07-22 00:20:22 +0800

    功能不变，优化代码。
    优化了views.py文件中的代码。
    更新了ORM，新增了两张表。
    更新了部分html文件的文件名。
    更新了url的映射规则。
    更新了数据库源文件。

--- LibrarySys/models.py
+++ LibrarySys/models.py
@@ -1,57 +1,78 @@
 from django.db import models
 
 
-class book_list(models.Model):
-    id = models.AutoField(primary_key=True)
-    isbn = models.CharField(max_length=15, null=False)
-    book_name = models.CharField(max_length=20, null=False)
-    author = models.CharField(max_length=100, null=False)
-    translator = models.CharField(max_length=40, null=True)
-    press = models.CharField(max_length=20, null=False)
-    price = models.FloatField(null=False)
-    owner = models.ForeignKey("user", on_delete=models.DO_NOTHING)
-    borrowed_times = models.IntegerField(default=0, null=False)
-    state_code = models.IntegerField(default=0, null=False)
-    profiles = models.CharField(max_length=300, null=True)
-    book_image = models.ImageField(upload_to="%Y/%m/%d/", null=True)
+class Book_list(models.Model):
+    """书籍信息表"""
+    id = models.AutoField(primary_key=True) # 这条记录的 id，也就是这本书的 id
+    isbn = models.CharField(max_length=15, null=False) # 书籍的 isbn
+    book_name = models.CharField(max_length=20, null=False) # 书籍名称
+    author = models.CharField(max_length=100, null=False) # 书籍作者
+    translator = models.CharField(max_length=40, null=True) # 书籍译者
+    press = models.CharField(max_length=20, null=False) # 书籍出版社
+    price = models.FloatField(null=False) # 书籍价格
+    owner = models.ForeignKey("User", on_delete=models.DO_NOTHING) # 书籍所有者的 id
+    borrowed_times = models.IntegerField(default=0, null=False) # 书籍被借次数
+    state_code = models.IntegerField(default=0, null=False) # 书籍状态码
+    profiles = models.CharField(max_length=300, null=True) # 书籍简介
+    book_image = models.ImageField(upload_to="%Y/%m/%d/", null=True) # 书籍图片
 
 
-class borrow(models.Model):
-    id = models.AutoField(primary_key=True)
-    book_name = models.ForeignKey("book_list", on_delete=models.DO_NOTHING)
-    end_time = models.DateTimeField(null=False)
-    previous = models.ForeignKey("user", on_delete=models.DO_NOTHING)
+class Borrow(models.Model):
+    """借阅成功记录表"""
+    id = models.AutoField(primary_key=True) # 这条记录的 id
+    book_name = models.ForeignKey("Book_list", on_delete=models.DO_NOTHING) # 被借书籍的 id
+    end_time = models.DateTimeField(null=False) # 借阅结束时间
+    previous = models.ForeignKey("User", on_delete=models.DO_NOTHING) # 上一个借阅者
 
 
-class login_record(models.Model):
-    id = models.AutoField(primary_key=True)
-    user = models.ForeignKey("user", on_delete=models.DO_NOTHING)
-    login_time = models.DateTimeField(null=False)
+class Login_record(models.Model):
+    """用户登录记录表"""
+    id = models.AutoField(primary_key=True) # 这条记录的 id
+    user = models.ForeignKey("User", on_delete=models.DO_NOTHING) # 登陆的用户的 id
+    login_time = models.DateTimeField(null=False) # 用户登录的时间
 
 
-class request(models.Model):
-    id = models.AutoField(primary_key=True)
-    book_name = models.ForeignKey("book_list", on_delete=models.DO_NOTHING)
-    cretime = models.DateTimeField(null=False)
-    requster = models.ForeignKey("user", on_delete=models.DO_NOTHING)
-    confirm_code = models.SmallIntegerField(default=0, null=False)
-    expiry_time = models.DateTimeField(null=False)
+class Request(models.Model):
+    id = models.AutoField(primary_key=True) # 这条记录的 id，也就是这条借阅请求的 id
+    book_name = models.ForeignKey("Book_list", on_delete=models.DO_NOTHING) # 请求借阅的书的 id
+    cretime = models.DateTimeField(null=False) # 请求创建时间
+    requster = models.ForeignKey("User", on_delete=models.DO_NOTHING) # 请求者
+    confirm_code = models.SmallIntegerField(default=0, null=False) # 请求状态码
+    expiry_time = models.DateTimeField(null=False) # 请求失效时间
 
 
-class user(models.Model):
-    id = models.AutoField(primary_key=True)
-    stu_id = models.CharField(max_length=12, null=False)
-    user_name = models.CharField(max_length=10, null=False)
-    password = models.CharField(max_length=16, null=False)
-    email = models.CharField(max_length=30, null=False)
-    phone = models.CharField(max_length=14, null=False)
-    holds = models.IntegerField(default=0, null=True)
-    cretime = models.DateTimeField(null=False)
-    last_time = models.DateTimeField(null=True)
-    cancellation = models.BooleanField(default=0, null=False)
+class User(models.Model):
+    """用户信息表"""
+    id = models.AutoField(primary_key=True) # 这条记录的 id，也是用户的 id，区分用户的第二选择
+    stu_id = models.CharField(max_length=12, null=False) # 用户学号，区分用户的第一选择
+    user_name = models.CharField(max_length=10, null=False) # 用户的用户名
+    password = models.CharField(max_length=16, null=False) # 用户密码
+    email = models.CharField(max_length=30, null=False) # 用户邮箱
+    phone = models.CharField(max_length=14, null=False) # 用户电话
+    holds = models.IntegerField(default=0, null=True) # 用户目前借了几本书
+    cretime = models.DateTimeField(null=False) # 账户建立时间
+    last_time = models.DateTimeField(null=True) # 账户最后登录时间
+    cancellation = models.BooleanField(default=0, null=False) # 账户是否已注销
 
 
+class Book_short_comment(models.Model):
+    """书籍简评表"""
+    id = models.AutoField(primary_key=True) # 这条记录的 id，也是每条简评的 id
+    book = models.ForeignKey("Book_list", on_delete=models.DO_NOTHING) # 书籍 id
+    commentator = models.ForeignKey("User", on_delete=models.DO_NOTHING) # 评论用户的 id
+    comment_text = models.TextField(max_length=300, null=False) # 评论正文
+    comment_time = models.DateTimeField(null=False) # 评论创建时间
+    like_num = models.IntegerField(default=0) # 喜欢数
+    unlike_num = models.IntegerField(default=0) # 不喜欢数
+    collect_num = models.IntegerField(default=0) # 收藏数
 
-# class book_list(models.Model):
-#     pass
+
+class Book_short_comment_like_and_collection_record(models.Model):
+    """书籍简评的点赞和收藏记录表"""
+    id = models.AutoField(primary_key=True) # 这条记录的 id，也是每条点赞收藏的 id
+    user = models.ForeignKey("User", on_delete=models.DO_NOTHING) # 记录归属的用户 id
+    comment = models.ForeignKey("Book_short_comment", on_delete=models.DO_NOTHING) # 评论 id
+    like_state = models.BooleanField(default=0, null=False) # 该用户是否喜欢这条简评
+    unlike_state = models.BooleanField(default=0, null=False) # 该用户是否不喜欢这条简评
+    collection_state = models.BooleanField(default=0, null=False) # 该用户是否收藏了这条简评
 
--- LibrarySys/views.py
+++ LibrarySys/views.py
@@ -1,119 +1,128 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 from django.http import HttpResponse, JsonResponse
 from django.views.generic import View
 from django.utils.timezone import now
-from .models import user, login_record, book_list
+from .models import User, Login_record, Book_list
+from django.views.generic import ListView
 
 
-def login(request):
-    """登陆视图函数"""
-    return render(request, 'login.html')
+class DomainView(View):
+    """域名视图类"""
+    def get(self, request, *args, **kwargs):
+        return redirect(to=reverse("login"), permanent=False)
+
+    def http_method_not_allowed(self, request, *args, **kwargs):
+        return HttpResponse("不支持除 get 之外的其他请求")
 
 
-def login_check(request):
-    """ajax 登录校验回调视图函数"""
-    # 1，获取用户输入的用户名和密码
-    stu_id = request.POST.get('stu_id')
-    password = request.POST.get('password')
-
-    # 2,用户名和密码校验以及记录登录时间
-    find_user = user.objects.filter(stu_id=stu_id, password=password)
-    if len(find_user) is 1:
-        # 先创建 response
-        response = JsonResponse({'msg': '2200'})
-        response.set_cookie(key='stu_id', value=stu_id, max_age=(4 * 60 * 60))
-        # 记录登录时间
-        login_time = now()
-        login_time_record = login_record(user=find_user[0], login_time=login_time)
-        login_time_record.save()
-
-        return response
-    else:
-        response = JsonResponse({'msg': '4499'})
-        return response
-
-
-def register(request):
-    """注册按钮视图函数"""
-    return render(request, 'register.html')
-
-
-def register_check(request):
-    """ajax 注册校验视图函数"""
-    # 从前端的 ajax 中获取注册所需数据
-    stu_id = request.POST.get('stu_id')
-    user_name = request.POST.get('user_name')
-    password = request.POST.get('password')
-    email = request.POST.get('email')
-    phone = request.POST.get('phone')
-    # 进行用户存在判断
-    signup_time = now()
-    find_user = user.objects.filter(stu_id=stu_id)
-    if len(find_user) is 0: # 查询到0条记录，即用户不存在
-        # 为用户注册
-        new_user = user(stu_id=stu_id, user_name=user_name, password=password,
-                        email=email, phone=phone, cretime=signup_time, last_time=signup_time)
-        new_user.save()
-        # 记录该次登录时间
-        last_registered_user = user.objects.filter(stu_id=stu_id) # 找到刚刚用这个学号注册的用户
-        login_time = signup_time
-        login_time_record = login_record(user=last_registered_user[0], login_time=login_time)
-        login_time_record.save()
-        # 创建 response
-        response = JsonResponse({'msg': '2200'})
-        response.set_cookie(key='stu_id', value=stu_id, max_age=(4*60*60))
-        return response
-    else:
-        return JsonResponse({'msg': "4400"})
-
-
-def index(request):
-    """首页视图函数"""
-    cookie = request.COOKIES
-    if len(cookie) is 0:
+class LoginView(View):
+    """登陆视图类"""
+    def get(self, request, *args, **kwargs):
         return render(request, 'login.html')
-    else:
-        context = {}
-        book_num = book_list.objects.all().count()
-        context["book_num"] = book_num
-        context["books"] = []
 
-        page_num = int(request.GET.get("page_num"))
-        book_id_num = [(page_num*8 - 7), (page_num*8 - 6), (page_num*8 - 5), (page_num*8 - 4),
-                       (page_num*8 - 3), (page_num*8 - 2), (page_num*8 - 1), (page_num*8)]
-        books = book_list.objects.filter(id__in=book_id_num)
-        for book in books:
-            book_detail = {}
-            book_detail["book_name"] = book.book_name
-            book_detail["author"] =  book.author
-            book_detail["translator"] = book.translator
-            book_detail["isbn"] = book.isbn
-            book_detail["press"] = book.press
-            book_detail["profiles"] = book.profiles
-            context["books"].append(book_detail)
-        print(context)
-        return render(request, 'index.html', context=context)
+    def post(self, request, *args, **kwargs):
+        """ajax 登录校验回调函数"""
+        # 1，获取用户输入的用户名和密码
+        stu_id = request.POST.get('stu_id')
+        password = request.POST.get('password')
+        # 2,用户名和密码校验以及记录登录时间
+        find_user = User.objects.filter(stu_id=stu_id, password=password)
+        if len(find_user) is 1:
+            # 先创建 response
+            response = JsonResponse({'msg': '2200'})
+            response.set_cookie(key='stu_id', value=stu_id, max_age=(4 * 60 * 60))
+            # 记录登录时间
+            login_time = now()
+            login_time_record = Login_record(user=find_user[0], login_time=login_time)
+            login_time_record.save()
+            return response
+        else:
+            response = JsonResponse({'msg': '4499'})
+            return response
+
+
+class RegisterView(View):
+    """注册视图类"""
+    def get(self, request, *args, **kwargs):
+        return render(request, 'register.html')
+
+    def post(self, request, *args, **kwargs):
+        """ajax 注册校验回调函数"""
+        # 从前端的 ajax 中获取注册所需数据
+        stu_id = request.POST.get('stu_id')
+        user_name = request.POST.get('user_name')
+        password = request.POST.get('password')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        # 进行用户存在判断
+        signup_time = now()
+        find_user = User.objects.filter(stu_id=stu_id)
+        if len(find_user) is 0:  # 查询到0条记录，即用户不存在
+            # 为用户注册
+            new_user = User(stu_id=stu_id, user_name=user_name, password=password,
+                            email=email, phone=phone, cretime=signup_time, last_time=signup_time)
+            new_user.save()
+            # 记录该次登录时间
+            last_registered_user = User.objects.filter(stu_id=stu_id)  # 找到刚刚用这个学号注册的用户
+            login_time = signup_time
+            login_time_record = Login_record(user=last_registered_user[0], login_time=login_time)
+            login_time_record.save()
+            # 创建 response
+            response = JsonResponse({'msg': '2200'})
+            response.set_cookie(key='stu_id', value=stu_id, max_age=(4 * 60 * 60))
+            return response
+        else:
+            return JsonResponse({'msg': "4400"})
+
+
+class BookListView(ListView):
+    """用来渲染book.html里的数据的列表视图类"""
+    model = Book_list
+    template_name = 'book.html'
+    paginate_by = 8
+    context_object_name = 'books'
+    ordering = 'id'
+    page_kwarg = 'page'
 
 
 def test(request):
     """测试用的，不用管"""
-    # find_user = user.objects.filter(stu_id='001', password='python')
+    # find_user = User.objects.filter(stu_id='001', password='python')
     # print(find_user[0].stu_id)
-    # find_user = user.objects.filter(stu_id="002", password="python")
+    # find_user = User.objects.filter(stu_id="002", password="python")
     # login_time = now()
-    # login_time_record = login_record(user=find_user[0], login_time=login_time)
+    # login_time_record = Login_record(user=find_user[0], login_time=login_time)
     # print(login_time)
     # login_time_record.save()
-    # dictionary = {'book_num': 3,
-    #               1: ['汇编语言 第三版', '王爽', None, '9787302333142', '清华大学出版社', None],
-    #               2: ['数据库系统概念 第六版', 'Abraham Silberschatz; Henry F. Korth; S. Sudarshan',
-    #                   '杨冬青 李红燕 唐世渭', '9787111400851', '机械工业出版社', None],
-    #               3: ['托马斯微积分 第十版', 'Finney; Weir; Giordano', '叶其孝 王耀东 唐兢', '9787040108231', '高等教育出版社', None]}
-    # for item in dictionary.items():
-    #     print(item)
-    #     # if item.key == "book_num":
-    #     #     pass
-    #     # else:
-    #     #     print(item.value)
-    return HttpResponse("shit....")
+    return HttpResponse("emmmmm... 你竟然无聊到了试这个....你注销账户吧...........")
+
+# 这是原版的首页视图函数，但是已经弃用了
+# class IndexView(View):
+#     def get(self, request, *args, **kwargs):
+#         cookie = request.COOKIES
+#         if len(cookie) is 0:
+#             return redirect(to=reverse("login"), permanent=False)
+#         else:
+#             context = {}
+#             book_num = Book_list.objects.all().count()
+#             context["book_num"] = book_num
+#             context["books"] = []
+#
+#             page_num = int(request.GET.get("page_num", default=1))
+#             book_id_num = [(page_num * 8 - 7), (page_num * 8 - 6), (page_num * 8 - 5), (page_num * 8 - 4),
+#                            (page_num * 8 - 3), (page_num * 8 - 2), (page_num * 8 - 1), (page_num * 8)]
+#             books = Book_list.objects.filter(id__in=book_id_num)
+#             for book in books:
+#                 book_detail = {}
+#                 book_detail["book_name"] = book.book_name
+#                 book_detail["author"] = book.author
+#                 book_detail["translator"] = book.translator
+#                 book_detail["isbn"] = book.isbn
+#                 book_detail["press"] = book.press
+#                 book_detail["profiles"] = book.profiles
+#                 context["books"].append(book_detail)
+#             return render(request, 'index.html', context=context)
+#
+#     def http_method_not_allowed(self, request, *args, **kwargs):
+#         return HttpResponse("不支持除 get 之外的其他请求")
 
